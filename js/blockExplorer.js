93aa81b9e8a9f97da410e6e2fef83d0fcdc09bb9
C7ement <34455787+C7ement@users.noreply.github.com>
2018-11-19 17:55:19 +0100

    began block explorer

--- js/blockExplorer.js
+++ js/blockExplorer.js
@@ -1,18 +1,50 @@
+function displayBlock(block) {
+    var table, td, tr, i;
+    table = document.getElementById('blockExplorer');
+    tr = document.createElement('tr');
+    td = document.createElement('td');
+    td.appendChild(document.createTextNode(block.height));
+    tr.appendChild(td);
+    table.appendChild(tr);
+}
+
+
+function displayAndGetNext(block) {
+    displayBlock(block);
+    return $.get(block.prev_block_url);
+}
+
+
+
 $(document).ready(function() {
-    $.ajax({
-        url : "https://blockchain.info/rawblock/$block_hash",
-        dataType : "json",
-        contentType : "application/json; charset=utf-8",
-        type : "GET",
-        timeout:	"5000",
-        async : false,
 
-        success : function(data) {
-            $('webService1').append(data.hash);
-        },
+    var table, th, tr, i, url, d;
+    d = document;
+    table = d.getElementById('blockExplorer');
+    table.className = "table";
+    tr = d.createElement('tr');
+    tr.className = "thead-light";
+    th = d.createElement('th');
+    th.appendChild(d.createTextNode('height'));
+    tr.appendChild(th);
+    th = d.createElement('th');
+    th.appendChild(d.createTextNode('Age'));
+    tr.appendChild(th);
+    th = d.createElement('th');
+    th.appendChild(d.createTextNode('fees'));
+    tr.appendChild(th);
+    th = d.createElement('th');
+    th.appendChild(d.createTextNode('peer_count'));
+    tr.appendChild(th);
+    table.appendChild(tr);
 
-        error : function(xhr, status, err) {
-            $('webService1').append(err+" N/A");
-        }
-    });
+    $.get("https://api.blockcypher.com/v1/btc/main").then(function(chain) {
+        return $.get(chain.latest_url);
+    }).then(displayAndGetNext).then(displayAndGetNext);
 });
+
+function createLine(blockData) {
+    for (i=0; i<10; i++) {
+    }
+    return blockData.prev_block_url;
+}
