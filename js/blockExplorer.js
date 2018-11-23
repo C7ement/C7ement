e4581363009a5a5130a1e6c9552b011dd92adb8d
C7ement <34455787+C7ement@users.noreply.github.com>
2018-11-23 23:23:34 +0100

    finished transaction and address display

--- js/blockExplorer.js
+++ js/blockExplorer.js
@@ -42,32 +42,77 @@ function displayBlock(block,tab) {
 function displayTransaction(tran,divT) {
 
     let date, i, div, html;
-
+    let somme = parseInt(tran.vin_sz)+parseInt(tran.vout_sz)+2;
     div = document.getElementById(divT);
 
     date = new Date(tran.received);
 
-    html = "<div class='row justify-content-center mt-5'><p class = 'font-weight-bold'>"+tran.hash+"</p></div>";
-    html += "<div class='row justify-content-center mt-4 mb-5'>";
-    html += "<table class='table tabTran table-bordered text-center w-auto col-auto my-auto'><thead><tr><td colspan='2'>Inputs</td></tr></thead><tbody>";
+    html = "<div class='row justify-content-center mt-4 mb-5'>"+
+        "<table class='table tabTran table-bordered text-center w-auto col-auto my-auto'>"+
+        "<tr><td class='align-middle' colspan='3'><b>"+tran.hash+"</b></td></tr>"+
+        "<tr>"+
+        "<td class='align-middle' rowspan='"+somme.toString()+"'>"+
+        "<p class='mt-3 mb-1'>"+date.toLocaleDateString()+"</p>"+
+        "<p class='mt-1 mb-3'>"+date.toLocaleTimeString()+"</p>"+
+        "<p class='mt-3 mb-1'>"+(tran.total/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" <a class='smCaps'>btc</a> sent</p>"+
+        "<p class='mt-1 mb-3'>Fees "+(tran.fees/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" <a class='smCaps'>btc</a></p>"+
+        "<p class='mt-3 mb-1'>Block "+tran.block_height+"</p>"+
+        "<p class='mt-1 mb-3'>"+tran.confirmations+" confirmations</p></td>"+
+        "<td class='align-middle bgG' colspan='2'>Inputs</td>"+
+        "</tr>";
     for (i=0; i<tran.inputs.length; i++) {
-        html += "<tr><td>"+tran.inputs[i].addresses[0]+"</td><td>"+(tran.inputs[i].output_value/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" BTC</td></tr>";
+        html += "<tr>"+
+            "<td class='align-middle'>"+tran.inputs[i].addresses[0]+"</td>"+
+            "<td class='align-middle'>"+
+            (tran.inputs[i].output_value/100000000).toLocaleString(undefined,{maximumSignificantDigits: 3})+" <a class='smCaps'>btc</a></td>"+
+            "</tr>";
     }
-    html += "</tbody>";
 
-    html += "<thead><tr><td colspan='2'>Outputs</td></tr></thead><tbody>";
+    html += "<tr><td class='align-middle bgG' colspan='2'>Outputs</td></tr>";
+
     for (i=0; i<tran.outputs.length; i++) {
-        html += "<tr><td>"+tran.outputs[i].addresses[0]+"</td><td>"+(tran.outputs[i].value/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" BTC</td></tr>";
+        html += "<tr>"+
+            "<td class='align-middle'>"+tran.outputs[i].addresses[0]+"</td>"+
+            "<td class='align-middle'>"+
+            (tran.outputs[i].value/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" <a class='smCaps'>btc</a></td>"+
+            "</tr>";
     }
-    html += "</tbody></table>";
-    html += "<div class='col-auto my-auto text-center'>";
-    html += "<p class='my-3'>"+date.toLocaleDateString()+"</p>";
-    html += "<p class='my-3'>"+date.toLocaleTimeString()+"</p>";
-    html += "<p class='my-3'>Sent "+(tran.total/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" BTC</p>";
-    html += "<p class='my-3'>Fees "+(tran.fees/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+" BTC</p>";
-    html += "<p class='my-3'>Block "+tran.block_height+"</p>";
-    html += "<p class='my-3'>Confirmed "+tran.confirmations+" times</p>";
-    html += "</div</div>";
+    html += "</table>";
+    html += "</div>";
+    div.innerHTML = html;
+}
+
+function displayBlock(block,tab) {
+    let tbody, tr, date;
+    date = new Date(block.received_time);
+    tbody = document.getElementById(tab).children[1];
+    tr = document.createElement('tr');
+    tr.appendChild(newTd(block.height));
+    tr.appendChild(newTd(date.toLocaleDateString()));
+    tr.appendChild(newTd(date.toLocaleTimeString()));
+    tr.appendChild(newTd(block.n_tx.toLocaleString()));
+    tr.appendChild(newTd((block.total/100000000).toLocaleString(undefined, {maximumFractionDigits: 0})+" BTC"));
+    tr.appendChild(newTd((block.fees/100000000).toLocaleString()+" BTC"));
+    tbody.appendChild(tr);
+}
+
+function displayAddress(addr,divT) {
+
+    let i, div, html;
+    div = document.getElementById(divT);
+
+
+    html = "<div class='row justify-content-center mt-4 mb-5'>"+
+        "<table class='table tabTran table-bordered text-center w-auto col-auto my-auto'>"+
+        "<tr><td class='align-middle bgG' colspan='4'><b>"+addr.address+"</b></td></tr>"+
+        "<tr>"+
+        "<td class='align-middle'>Received<br>"+(addr.total_received/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+"<a class='smCaps'> btc</a></td>"+
+        "<td class='align-middle'>Sent<br>"+(addr.total_sent/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+"<a class='smCaps'> btc</a></td>"+
+        "<td class='align-middle'>Balance<br>"+(addr.balance/100000000).toLocaleString(undefined, {maximumSignificantDigits: 3})+"<a class='smCaps'> btc</a></td>"+
+        "<td class='align-middle'>Transactions<br>"+addr.n_tx+"</td>"+
+        "</tr>";
+    html += "</table>";
+    html += "</div>";
     div.innerHTML = html;
 }
 
@@ -85,6 +130,9 @@ function displayInTran1(tran) {
     displayTransaction(tran,'tran1');
 }
 
+function displayAddressInToto(address) {
+    displayAddress(address,'address1')
+}
 
 function latestBlocks() {/*
 
@@ -110,4 +158,8 @@ function searchTransaction() {
     $.get("https://api.blockcypher.com/v1/btc/main/txs/"+x.value).then(displayInTran1);
     /*$.get("https://api.blockcypher.com/v1/btc/main/txs/5404e339ba82e6e52fcc24cb40029bed8425baa4c7f869626ef9de956186f910").then(displayInTran1);*/
     /*scrollto*/
+}
+
+function searchAddress() {
+    $.get('https://api.blockcypher.com/v1/btc/main/addrs/1DEP8i3QJCsomS4BSMY2RpU1upv62aGvhD/balance').then(displayAddressInToto);
 }
